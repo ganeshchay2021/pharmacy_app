66213be773e9ea72225f3ea7c978fd2fd4469efd
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-03-23 21:15:30 +0545

    Feat(View): Home View UI Added

---
+++ lib/view/home_view.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:get/get.dart';
+import 'package:pharmacy_app/controller/route_controller.dart';
+import 'package:pharmacy_app/utils/constants.dart';
+import 'package:pharmacy_app/utils/text_style_widget.dart';
+import 'package:pharmacy_app/widgets/category.dart';
+import 'package:pharmacy_app/widgets/medicine_list.dart';
+import 'package:pharmacy_app/widgets/search_field.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Constants.backgrondColor,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.zero,
+        child: Container(
+          padding: EdgeInsets.only(top: 50, left: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //user image
+              ClipRRect(
+                borderRadius: BorderRadius.circular(60),
+                child: Image.asset(
+                  Constants.user,
+                  width: 80,
+                  height: 80,
+                  fit: BoxFit.cover,
+                ),
+              ),
+      
+              Gap(Constants.spaceBwtItems),
+      
+              //heading text
+              Text(
+                "Your Trusted",
+                style: TextStyleWidget.headlineTextStyle(20),
+              ),
+      
+              //light text
+              Text(
+                "Online Pharmacy",
+                style: TextStyleWidget.lightTextStyle(22),
+              ),
+      
+              Gap(Constants.spaceBwtSections),
+      
+              //search field
+              SearchField(),
+      
+              Gap(Constants.spaceBwtSections),
+      
+              //categories list
+              Obx(
+                () => SizedBox(
+                  height: 50,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: List.generate(Constants.categoriesList.length, (
+                      index,
+                    ) {
+                      return GestureDetector(
+                        onTap: () {
+                          Get.find<RouteController>().selectedIndex.value =
+                              index;
+                        },
+                        child: Category(
+                          title: Constants.categoriesList[index],
+                          selectedIndex:
+                              Get.find<RouteController>().selectedIndex.value,
+                          index: index,
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+              ),
+      
+              Gap(Constants.spaceBwtSections),
+      
+              //medicines list
+              MedicineList(
+                onTap: () {
+                  // Handle medicine tap
+                  Get.toNamed("/product");
+                },
+              ),
+
+              MedicineList(
+                onTap: () {
+                  // Handle medicine tap
+                  Get.toNamed("/product");
+                },
+              ),
+
+              MedicineList(
+                onTap: () {
+                  // Handle medicine tap
+                  Get.toNamed("/product");
+                },
+              ),
+      
+              
+      
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
