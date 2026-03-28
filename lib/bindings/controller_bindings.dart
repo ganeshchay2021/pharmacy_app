13681c93360fc8419e628eed914dc28e12c03790
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2026-03-28 20:24:49 +0545

    Feat(bindings) : Controller Bindings Updated with others controller

--- lib/bindings/controller_bindings.dart
+++ lib/bindings/controller_bindings.dart
@@ -1,9 +1,15 @@
 import 'package:get/get.dart';
+import 'package:pharmacy_app/controller/admin_comtroller.dart';
 import 'package:pharmacy_app/controller/auth_controller.dart';
+import 'package:pharmacy_app/controller/product_controller.dart';
 
 class ControllerBindings extends Bindings {
   @override
   void dependencies() {
     Get.put<AuthController>(AuthController(), permanent: true);
+    Get.put<AdminController>(AdminController(), permanent: true);
+    Get.put<ProductController>(ProductController(), permanent: true);
+
+
   }
 }
